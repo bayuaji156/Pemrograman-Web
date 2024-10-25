aae31c4e05d6ccfc4af3930a8b92241645bff106
bayuaji156 <149068519+bayuaji156@users.noreply.github.com>
2024-10-25 20:07:55 +0700

    Update Segitiga1.php

--- segitiga/Segitiga1.php
+++ segitiga/Segitiga1.php
@@ -1,1 +1,22 @@
+<?php
+echo "<pre>"; 
+$tinggi = 5; 
+
+for ($i = 1; $i <= $tinggi; $i++) {
+    
+    for ($j = $tinggi; $j > $i; $j--) {
+        echo " ";
+    }
+    
+    for ($k = 1; $k <= (2 * $i - 1); $k++) {
+        echo "*";
+    }
+    echo "\n";
+}
+echo "</pre>";
+?>
+
+
+
+
 
